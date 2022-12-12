1a1341f467d826217076be4ab336794d1c9f4a06
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-12 15:18:35 +0100

    feat: add Morpho contract

---
+++ src/Morpho.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "openzeppelin-contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract Morpho is ERC1155Upgradeable, OwnableUpgradeable {}
