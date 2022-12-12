bc425d28f9e7ff7fc80486f5b97b8c3bdce4fc1a
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-12 15:22:48 +0100

    style: explicit import

--- src/Morpho.sol
+++ src/Morpho.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import {Events} from "./libraries/Events.sol";
 import {Errors} from "./libraries/Errors.sol";
 
-import "openzeppelin-contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
-import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ERC1155Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 contract Morpho is ERC1155Upgradeable, OwnableUpgradeable {}
