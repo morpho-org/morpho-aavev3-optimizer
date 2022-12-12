69a27d4606855dc23a230fd5ea66cc24fd2b2b63
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-12 17:50:23 +0100

    feat: add entry points templates

--- src/Morpho.sol
+++ src/Morpho.sol
@@ -7,4 +7,31 @@ import {Errors} from "./libraries/Errors.sol";
 import {ERC1155Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract Morpho is ERC1155Upgradeable, OwnableUpgradeable {}
+contract Morpho is ERC1155Upgradeable, OwnableUpgradeable {
+    /// EXTERNAL ///
+
+    function supply(address _underlying, uint256 _amount, address _from, address _to, uint256 _nbOfLoops)
+        external
+        returns (uint256 supplied)
+    {}
+
+    function borrow(address _underlying, uint256 _amount, address _from, address _to, uint256 _nbOfLoops)
+        external
+        returns (uint256 borrowed)
+    {}
+
+    function repay(address _underlying, uint256 _amount, address _from, address _to, uint256 _nbOfLoops)
+        external
+        returns (uint256 repaid)
+    {}
+
+    function witdhraw(address _underlying, uint256 _amount, address _from, address _to, uint256 _nbOfLoops)
+        external
+        returns (uint256 withdrawn)
+    {}
+
+    function liquidate(address _collateralUnderlying, address _borrowedUnderlying, address _user, uint256 _amount)
+        external
+        returns (uint256 repaid, uint256 seized)
+    {}
+}
