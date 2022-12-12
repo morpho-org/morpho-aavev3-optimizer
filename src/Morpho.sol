5b1c048a5fab49d8e14ce9e03f54f014cf71e2ce
MerlinEgalite <egalite.merlin@gmail.com>
2022-12-12 15:21:52 +0100

    feat: create Errors and Events libs

--- src/Morpho.sol
+++ src/Morpho.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import {Events} from "./libraries/Events.sol";
+import {Errors} from "./libraries/Errors.sol";
+
 import "openzeppelin-contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 
