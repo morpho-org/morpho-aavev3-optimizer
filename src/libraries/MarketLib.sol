07c1a56a629ac4df8cb3d1ee0674a0d520e0201b
Patrick Kim <patrick@morpho.xyz>
2022-12-19 21:42:15 -0500

    chore: format

--- src/libraries/MarketLib.sol
+++ src/libraries/MarketLib.sol
@@ -1,51 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import { Types } from "./Types.sol";
-import { ThreeHeapOrdering } from "morpho-data-structures/ThreeHeapOrdering.sol";
-import { SafeCastUpgradeable as SafeCast } from "@openzeppelin-contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
+import {Types} from "./Types.sol";
+import {ThreeHeapOrdering} from "morpho-data-structures/ThreeHeapOrdering.sol";
+import {SafeCastUpgradeable as SafeCast} from "@openzeppelin-contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
 
 library MarketLib {
-  using SafeCast for uint256;
+    using SafeCast for uint256;
 
-  function isCreated(Types.Market storage market) internal view returns (bool) {
-    return market.underlying != address(0);
-  }
+    function isCreated(Types.Market storage market) internal view returns (bool) {
+        return market.underlying != address(0);
+    }
 
-  function isCreatedMem(
-    Types.Market memory market
-  ) internal pure returns (bool) {
-    return market.underlying != address(0);
-  }
+    function isCreatedMem(Types.Market memory market) internal pure returns (bool) {
+        return market.underlying != address(0);
+    }
 
-  function getIndexes(
-    Types.Market storage market
-  )
-    internal
-    view
-    returns (
-      uint256 poolSupplyIndex,
-      uint256 poolBorrowIndex,
-      uint256 p2pSupplyIndex,
-      uint256 p2pBorrowIndex
-    )
-  {
-    poolSupplyIndex = uint256(market.poolSupplyIndex);
-    poolBorrowIndex = uint256(market.poolBorrowIndex);
-    p2pSupplyIndex = uint256(market.p2pSupplyIndex);
-    p2pBorrowIndex = uint256(market.p2pBorrowIndex);
-  }
+    function getIndexes(Types.Market storage market)
+        internal
+        view
+        returns (uint256 poolSupplyIndex, uint256 poolBorrowIndex, uint256 p2pSupplyIndex, uint256 p2pBorrowIndex)
+    {
+        poolSupplyIndex = uint256(market.poolSupplyIndex);
+        poolBorrowIndex = uint256(market.poolBorrowIndex);
+        p2pSupplyIndex = uint256(market.p2pSupplyIndex);
+        p2pBorrowIndex = uint256(market.p2pBorrowIndex);
+    }
 
-  function setIndexes(
-    Types.Market storage market,
-    uint256 poolSupplyIndex,
-    uint256 poolBorrowIndex,
-    uint256 p2pSupplyIndex,
-    uint256 p2pBorrowIndex
-  ) internal {
-    market.poolSupplyIndex = poolSupplyIndex.toUint128();
-    market.poolBorrowIndex = poolBorrowIndex.toUint128();
-    market.p2pSupplyIndex = p2pSupplyIndex.toUint128();
-    market.p2pBorrowIndex = p2pBorrowIndex.toUint128();
-  }
+    function setIndexes(
+        Types.Market storage market,
+        uint256 poolSupplyIndex,
+        uint256 poolBorrowIndex,
+        uint256 p2pSupplyIndex,
+        uint256 p2pBorrowIndex
+    ) internal {
+        market.poolSupplyIndex = poolSupplyIndex.toUint128();
+        market.poolBorrowIndex = poolBorrowIndex.toUint128();
+        market.p2pSupplyIndex = p2pSupplyIndex.toUint128();
+        market.p2pBorrowIndex = p2pBorrowIndex.toUint128();
+    }
 }
--- src/libraries/Types.sol
+++ src/libraries/Types.sol
@@ -1,120 +1,120 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import { ThreeHeapOrdering } from "morpho-data-structures/ThreeHeapOrdering.sol";
+import {ThreeHeapOrdering} from "morpho-data-structures/ThreeHeapOrdering.sol";
 
 library Types {
-  /// ENUMS ///
+    /// ENUMS ///
 
-  enum PositionType {
-    SUPPLY,
-    COLLATERAL,
-    BORROW
-  }
+    enum PositionType {
+        SUPPLY,
+        COLLATERAL,
+        BORROW
+    }
 
-  /// NESTED STRUCTS ///
+    /// NESTED STRUCTS ///
 
-  struct UserMarkets {
-    bytes32 data;
-  }
+    struct UserMarkets {
+        bytes32 data;
+    }
 
-  struct BorrowMask {
-    bytes32 data;
-  }
+    struct BorrowMask {
+        bytes32 data;
+    }
 
-  struct Delta {
-    uint256 p2pSupplyDelta; // Difference between the stored peer-to-peer supply amount and the real peer-to-peer supply amount (in pool supply unit).
-    uint256 p2pBorrowDelta; // Difference between the stored peer-to-peer borrow amount and the real peer-to-peer borrow amount (in pool borrow unit).
-    uint256 p2pSupplyAmount; // Sum of all stored peer-to-peer supply (in peer-to-peer supply unit).
-    uint256 p2pBorrowAmount; // Sum of all stored peer-to-peer borrow (in peer-to-peer borrow unit).
-  }
+    struct Delta {
+        uint256 p2pSupplyDelta; // Difference between the stored peer-to-peer supply amount and the real peer-to-peer supply amount (in pool supply unit).
+        uint256 p2pBorrowDelta; // Difference between the stored peer-to-peer borrow amount and the real peer-to-peer borrow amount (in pool borrow unit).
+        uint256 p2pSupplyAmount; // Sum of all stored peer-to-peer supply (in peer-to-peer supply unit).
+        uint256 p2pBorrowAmount; // Sum of all stored peer-to-peer borrow (in peer-to-peer borrow unit).
+    }
 
-  struct PauseStatuses {
-    bool isP2PDisabled;
-    bool isSupplyPaused;
-    bool isBorrowPaused;
-    bool isWithdrawPaused;
-    bool isRepayPaused;
-    bool isLiquidateCollateralPaused;
-    bool isLiquidateBorrowPaused;
-    bool isDeprecated;
-  }
+    struct PauseStatuses {
+        bool isP2PDisabled;
+        bool isSupplyPaused;
+        bool isBorrowPaused;
+        bool isWithdrawPaused;
+        bool isRepayPaused;
+        bool isLiquidateCollateralPaused;
+        bool isLiquidateBorrowPaused;
+        bool isDeprecated;
+    }
 
-  /// STORAGE STRUCTS ///
+    /// STORAGE STRUCTS ///
 
-  // This market struct is able to be passed into memory.
-  struct Market {
-    // SLOT 0-1
-    uint128 poolSupplyIndex; // 128 bits
-    uint128 poolBorrowIndex; // 128 bits
-    uint128 p2pSupplyIndex; // 128 bits
-    uint128 p2pBorrowIndex; // 128 bits
-    // SLOT 2
-    BorrowMask borrowMask; // 256 bits
-    // SLOT 3-6
-    Delta deltas; // 1024 bits
-    // SLOT 7
-    address underlying; // 168 bits
-    PauseStatuses pauseStatuses; // 64 bits
-    // SLOT 8
-    address variableDebtToken; // 168 bits
-    uint32 lastUpdateTimestamp; // 32 bits
-    uint16 reserveFactor; // 16 bits
-    uint16 p2pIndexCursor; // 16 bits
-  }
+    // This market struct is able to be passed into memory.
+    struct Market {
+        // SLOT 0-1
+        uint128 poolSupplyIndex; // 128 bits
+        uint128 poolBorrowIndex; // 128 bits
+        uint128 p2pSupplyIndex; // 128 bits
+        uint128 p2pBorrowIndex; // 128 bits
+        // SLOT 2
+        BorrowMask borrowMask; // 256 bits
+        // SLOT 3-6
+        Delta deltas; // 1024 bits
+        // SLOT 7
+        address underlying; // 168 bits
+        PauseStatuses pauseStatuses; // 64 bits
+        // SLOT 8
+        address variableDebtToken; // 168 bits
+        uint32 lastUpdateTimestamp; // 32 bits
+        uint16 reserveFactor; // 16 bits
+        uint16 p2pIndexCursor; // 16 bits
+    }
 
-  // Contains storage-only dynamic arrays and mappings.
-  struct MarketBalances {
-    ThreeHeapOrdering.HeapArray p2pSuppliers; // in scaled unit
-    ThreeHeapOrdering.HeapArray poolSuppliers; // in scaled unit
-    ThreeHeapOrdering.HeapArray p2pBorrowers; // in scaled unit
-    ThreeHeapOrdering.HeapArray poolBorrowers; // in scaled unit
-    mapping(address => uint256) collateral; // in scaled unit
-  }
+    // Contains storage-only dynamic arrays and mappings.
+    struct MarketBalances {
+        ThreeHeapOrdering.HeapArray p2pSuppliers; // in scaled unit
+        ThreeHeapOrdering.HeapArray poolSuppliers; // in scaled unit
+        ThreeHeapOrdering.HeapArray p2pBorrowers; // in scaled unit
+        ThreeHeapOrdering.HeapArray poolBorrowers; // in scaled unit
+        mapping(address => uint256) collateral; // in scaled unit
+    }
 
-  /// STACK AND RETURN STRUCTS ///
+    /// STACK AND RETURN STRUCTS ///
 
-  struct AssetLiquidityData {
-    uint256 decimals; // The number of decimals of the underlying token.
-    uint256 tokenUnit; // The token unit considering its decimals.
-    uint256 liquidationThreshold; // The liquidation threshold applied on this token (in basis point).
-    uint256 ltv; // The LTV applied on this token (in basis point).
-    uint256 underlyingPrice; // The price of the token (In base currency in wad).
-  }
+    struct AssetLiquidityData {
+        uint256 decimals; // The number of decimals of the underlying token.
+        uint256 tokenUnit; // The token unit considering its decimals.
+        uint256 liquidationThreshold; // The liquidation threshold applied on this token (in basis point).
+        uint256 ltv; // The LTV applied on this token (in basis point).
+        uint256 underlyingPrice; // The price of the token (In base currency in wad).
+    }
 
-  struct LiquidityData {
-    uint256 collateral; // The collateral value (In base currency in wad).
-    uint256 maxDebt; // The max debt value (In base currency in wad).
-    uint256 liquidationThresholdValue; // The liquidation threshold value (In base currency in wad).
-    uint256 debt; // The debt value (In base currency in wad).
-  }
+    struct LiquidityData {
+        uint256 collateral; // The collateral value (In base currency in wad).
+        uint256 maxDebt; // The max debt value (In base currency in wad).
+        uint256 liquidationThresholdValue; // The liquidation threshold value (In base currency in wad).
+        uint256 debt; // The debt value (In base currency in wad).
+    }
 
-  struct MatchVars {
-    address poolToken;
-    uint256 poolIndex;
-    uint256 p2pIndex;
-    uint256 amount;
-    uint256 maxLoops;
-    bool borrow;
-    bool matching; // True for match, False for unmatch
-  }
+    struct MatchVars {
+        address poolToken;
+        uint256 poolIndex;
+        uint256 p2pIndex;
+        uint256 amount;
+        uint256 maxLoops;
+        bool borrow;
+        bool matching; // True for match, False for unmatch
+    }
 
-  struct IRMParams {
-    uint256 lastPoolSupplyIndex;
-    uint256 lastPoolBorrowIndex;
-    uint256 lastP2PSupplyIndex;
-    uint256 lastP2PBorrowIndex;
-    uint256 poolSupplyIndex; // The current pool supply index.
-    uint256 poolBorrowIndex; // The current pool borrow index.
-    uint256 reserveFactor; // The reserve factor percentage (10 000 = 100%).
-    uint256 p2pIndexCursor; // The peer-to-peer index cursor (10 000 = 100%).
-    Delta deltas; // The deltas and peer-to-peer amounts.
-  }
+    struct IRMParams {
+        uint256 lastPoolSupplyIndex;
+        uint256 lastPoolBorrowIndex;
+        uint256 lastP2PSupplyIndex;
+        uint256 lastP2PBorrowIndex;
+        uint256 poolSupplyIndex; // The current pool supply index.
+        uint256 poolBorrowIndex; // The current pool borrow index.
+        uint256 reserveFactor; // The reserve factor percentage (10 000 = 100%).
+        uint256 p2pIndexCursor; // The peer-to-peer index cursor (10 000 = 100%).
+        Delta deltas; // The deltas and peer-to-peer amounts.
+    }
 
-  struct GrowthFactors {
-    uint256 poolSupplyGrowthFactor; // The pool's supply index growth factor (in ray).
-    uint256 poolBorrowGrowthFactor; // The pool's borrow index growth factor (in ray).
-    uint256 p2pSupplyGrowthFactor; // Peer-to-peer supply index growth factor (in ray).
-    uint256 p2pBorrowGrowthFactor; // Peer-to-peer borrow index growth factor (in ray).
-  }
+    struct GrowthFactors {
+        uint256 poolSupplyGrowthFactor; // The pool's supply index growth factor (in ray).
+        uint256 poolBorrowGrowthFactor; // The pool's borrow index growth factor (in ray).
+        uint256 p2pSupplyGrowthFactor; // Peer-to-peer supply index growth factor (in ray).
+        uint256 p2pBorrowGrowthFactor; // Peer-to-peer borrow index growth factor (in ray).
+    }
 }
