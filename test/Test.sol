bab0745c103a943173748c8f40973ac535fd9c03
Rubilmax <rmilon@gmail.com>
2023-01-03 11:52:27 +0100

    test(integration): add integration tests

--- test/Test.sol
+++ test/Test.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 import {TestHelpers} from "./helpers/TestHelpers.sol";
-import {console2} from "@forge-std/console2.sol";
 import {IPool, IPoolAddressesProvider} from "../src/interfaces/aave/IPool.sol";
 
 import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
@@ -14,36 +13,62 @@ import {Morpho} from "../src/Morpho.sol";
 
 import {Types} from "../src/libraries/Types.sol";
 
-import {Test} from "@forge-std/Test.sol";
+import {Test as ForgeTest} from "@forge-std/Test.sol";
+import {console2} from "@forge-std/console2.sol";
 
-contract TestSetup is Test {
-    // Common test variables between all networks
-    IPoolAddressesProvider public addressesProvider;
-    IPool public pool;
-    address public dai;
-    address public usdc;
-    address public usdt;
-    address public wbtc;
-    address public wNative;
+contract Test is ForgeTest {
+    IPool pool;
+    IPoolAddressesProvider addressesProvider;
+    ProxyAdmin proxyAdmin;
 
-    EntryPositionsManager public entryPositionsManager;
-    ExitPositionsManager public exitPositionsManager;
-    Morpho public morphoImplementation;
-    TransparentUpgradeableProxy public morphoProxy;
-    ProxyAdmin public proxyAdmin;
-    Morpho public morpho;
+    address dai;
+    address usdc;
+    address usdt;
+    address wbtc;
+    address wNative;
 
-    // The full list of markets to be tested when fuzzing or looping through all markets
-    address[] public markets;
+    Morpho morphoImpl;
 
-    uint256 public forkId;
+    Morpho morpho;
+    EntryPositionsManager entryPositionsManager;
+    ExitPositionsManager exitPositionsManager;
 
-    function setUp() public {
-        configSetUp();
-        deployAndSet();
+    TransparentUpgradeableProxy morphoProxy;
+
+    address[] testMarkets;
+
+    struct TestMarket {
+        address poolToken;
+        address debtToken;
+        address underlying;
+        string symbol;
+        uint256 decimals;
+        uint256 ltv;
+        uint256 liquidationThreshold;
+        Types.Market config;
+        //
+        bool isActive;
+        bool isFrozen;
+        //
+        Types.PauseStatuses status;
     }
 
-    function configSetUp() public {
+    TestMarket[] public markets;
+    TestMarket[] public activeMarkets;
+    TestMarket[] public unpausedMarkets;
+    TestMarket[] public collateralMarkets;
+    TestMarket[] public borrowableMarkets;
+    TestMarket[] public borrowableCollateralMarkets;
+
+    uint256 forkId;
+    uint256 snapshotId = type(uint256).max;
+
+    function setUp() public virtual {
+        _configSetUp();
+        _deployAndSet();
+    }
+
+    function _configSetUp() public {
         string memory network = vm.envString("NETWORK");
         string memory config = TestHelpers.getJsonConfig(network);
 
@@ -52,22 +77,24 @@ contract TestSetup is Test {
         addressesProvider =
             IPoolAddressesProvider(TestHelpers.getAddressFromJson(config, "LendingPoolAddressesProvider"));
         pool = IPool(addressesProvider.getPool());
+
         dai = TestHelpers.getAddressFromJson(config, "DAI");
         usdc = TestHelpers.getAddressFromJson(config, "USDC");
         usdt = TestHelpers.getAddressFromJson(config, "USDT");
         wbtc = TestHelpers.getAddressFromJson(config, "WBTC");
         wNative = TestHelpers.getAddressFromJson(config, "wrappedNative");
 
-        markets = TestHelpers.getTestMarkets(config);
+        testMarkets = TestHelpers.getTestMarkets(config);
     }
 
-    function deployAndSet() public {
+    function _deployAndSet() public {
+        proxyAdmin = new ProxyAdmin();
+
+        morphoImpl = new Morpho();
         entryPositionsManager = new EntryPositionsManager();
         exitPositionsManager = new ExitPositionsManager();
-        morphoImplementation = new Morpho();
 
-        proxyAdmin = new ProxyAdmin();
-        morphoProxy = new TransparentUpgradeableProxy(payable(address(morphoImplementation)), address(proxyAdmin), "");
+        morphoProxy = new TransparentUpgradeableProxy(payable(address(morphoImpl)), address(proxyAdmin), "");
         morpho = Morpho(payable(address(morphoProxy)));
 
         morpho.initialize(
@@ -78,17 +105,12 @@ contract TestSetup is Test {
             20
         );
 
-        createMarket(dai);
-    }
-
-    function createMarket(address underlying) public {
-        morpho.createMarket(underlying, 0, 3_333);
-    }
-
-    function testTest() public view {
-        console2.log("test");
-        for (uint256 i; i < markets.length; i++) {
-            console2.log(markets[i]);
+        for (uint256 i; i < testMarkets.length; ++i) {
+            _createMarket(testMarkets[i]);
         }
+    }
+
+    function _createMarket(address underlying) public {
+        morpho.createMarket(underlying, 0, 3_333);
     }
 }
