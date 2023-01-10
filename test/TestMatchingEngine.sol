85cca12b3bb0d29e28b0cd348d5e0125ac8fbfcf
Patrick Kim <patrick@morpho.xyz>
2023-01-09 21:34:19 -0500

    feat: add zero remaining check and add matching engine tests

--- test/TestMatchingEngine.sol
+++ test/TestMatchingEngine.sol
@@ -28,7 +28,7 @@ import {MarketBalanceLib} from "../src/libraries/MarketBalanceLib.sol";
 import {PoolLib} from "../src/libraries/PoolLib.sol";
 import {Math} from "@morpho-utils/math/Math.sol";
 
-contract TestMorphoInternal is TestSetup, MatchingEngine {
+contract TestMatchingEngine is TestSetup, MatchingEngine {
     using MarketLib for Types.Market;
     using MarketBalanceLib for Types.MarketBalances;
     using PoolLib for IPool;
@@ -44,14 +44,14 @@ contract TestMorphoInternal is TestSetup, MatchingEngine {
     constructor() TestSetup() MorphoStorage(config.load(vm.envString("NETWORK")).getAddress("addressesProvider")) {}
 
     function setUp() public virtual override {
-        _defaultMaxLoops = Types.MaxLoops(10, 10, 10, 10);
-        _maxSortedUsers = 20;
-
-        createMarket(dai);
-        createMarket(wbtc);
-        createMarket(usdc);
-        createMarket(usdt);
-        createMarket(wNative);
+        super.setUp();
+        _market[dai].setIndexes(
+            Types.Indexes256(
+                Types.MarketSideIndexes256(WadRayMath.RAY, WadRayMath.RAY),
+                Types.MarketSideIndexes256(WadRayMath.RAY, WadRayMath.RAY)
+            )
+        );
+        _maxSortedUsers = 10;
     }
 
     function testPromote(
@@ -64,7 +64,7 @@ contract TestMorphoInternal is TestSetup, MatchingEngine {
         poolBalance = bound(poolBalance, 0, type(uint96).max);
         p2pBalance = bound(p2pBalance, 0, type(uint96).max);
         poolIndex = bound(poolIndex, WadRayMath.RAY, WadRayMath.RAY * 1_000);
-        poolIndex = bound(poolIndex, WadRayMath.RAY, WadRayMath.RAY * 1_000);
+        p2pIndex = bound(p2pIndex, WadRayMath.RAY, WadRayMath.RAY * 1_000);
         remaining = bound(remaining, 0, type(uint96).max);
 
         uint256 toProcess = Math.min(poolBalance.rayMul(poolIndex), remaining);
@@ -75,5 +75,154 @@ contract TestMorphoInternal is TestSetup, MatchingEngine {
         assertEq(newPoolBalance, poolBalance - toProcess.rayDiv(poolIndex));
         assertEq(newP2PBalance, p2pBalance + toProcess.rayDiv(p2pIndex));
         assertEq(newRemaining, remaining - toProcess);
+    }
+
+    function testDemote(uint256 poolBalance, uint256 p2pBalance, uint256 poolIndex, uint256 p2pIndex, uint256 remaining)
+        public
+    {
+        poolBalance = bound(poolBalance, 0, type(uint96).max);
+        p2pBalance = bound(p2pBalance, 0, type(uint96).max);
+        poolIndex = bound(poolIndex, WadRayMath.RAY, WadRayMath.RAY * 1_000);
+        p2pIndex = bound(p2pIndex, WadRayMath.RAY, WadRayMath.RAY * 1_000);
+        remaining = bound(remaining, 0, type(uint96).max);
+
+        uint256 toProcess = Math.min(p2pBalance.rayMul(p2pIndex), remaining);
+
+        (uint256 newPoolBalance, uint256 newP2PBalance, uint256 newRemaining) =
+            _demote(poolBalance, p2pBalance, Types.MarketSideIndexes256(poolIndex, p2pIndex), remaining);
+
+        assertEq(newPoolBalance, poolBalance + toProcess.rayDiv(poolIndex));
+        assertEq(newP2PBalance, p2pBalance - toProcess.rayDiv(p2pIndex));
+        assertEq(newRemaining, remaining - toProcess);
+    }
+
+    function testPromoteSuppliers(uint256 numSuppliers, uint256 amountToMatch, uint256 maxLoops) public {
+        numSuppliers = bound(numSuppliers, 0, 10);
+        amountToMatch = bound(amountToMatch, 0, 20e18);
+        maxLoops = bound(maxLoops, 0, numSuppliers);
+
+        Types.MarketBalances storage marketBalances = _marketBalances[dai];
+
+        for (uint256 i; i < numSuppliers; i++) {
+            _updateSupplierInDS(dai, address(uint160(i + 1)), 1e18, 0);
+        }
+
+        (uint256 promoted, uint256 loopsDone) = _promoteSuppliers(dai, amountToMatch, maxLoops);
+
+        uint256 totalP2PSupply;
+        for (uint256 i; i < numSuppliers; i++) {
+            address user = address(uint160(i + 1));
+            assertEq(
+                marketBalances.scaledPoolSupplyBalance(user) + marketBalances.scaledP2PSupplyBalance(user),
+                1e18,
+                "user supply"
+            );
+            totalP2PSupply += marketBalances.scaledP2PSupplyBalance(user);
+        }
+
+        uint256 expectedPromoted = Math.min(amountToMatch, maxLoops * 1e18);
+        expectedPromoted = Math.min(expectedPromoted, numSuppliers * 1e18);
+
+        uint256 expectedLoops = Math.min(expectedPromoted.divUp(1e18), maxLoops);
+
+        assertEq(promoted, expectedPromoted, "promoted");
+        assertEq(totalP2PSupply, promoted, "total borrow");
+        assertEq(loopsDone, expectedLoops, "loops");
+    }
+
+    function testPromoteBorrowers(uint256 numBorrowers, uint256 amountToMatch, uint256 maxLoops) public {
+        numBorrowers = bound(numBorrowers, 0, 10);
+        amountToMatch = bound(amountToMatch, 0, 20e18);
+        maxLoops = bound(maxLoops, 0, numBorrowers);
+
+        Types.MarketBalances storage marketBalances = _marketBalances[dai];
+
+        for (uint256 i; i < numBorrowers; i++) {
+            _updateBorrowerInDS(dai, address(uint160(i + 1)), 1e18, 0);
+        }
+
+        (uint256 promoted, uint256 loopsDone) = _promoteBorrowers(dai, amountToMatch, maxLoops);
+
+        uint256 totalP2PBorrow;
+        for (uint256 i; i < numBorrowers; i++) {
+            address user = address(uint160(i + 1));
+            assertEq(
+                marketBalances.scaledPoolBorrowBalance(user) + marketBalances.scaledP2PBorrowBalance(user),
+                1e18,
+                "user borrow"
+            );
+            totalP2PBorrow += marketBalances.scaledP2PBorrowBalance(user);
+        }
+
+        uint256 expectedPromoted = Math.min(amountToMatch, maxLoops * 1e18);
+        expectedPromoted = Math.min(expectedPromoted, numBorrowers * 1e18);
+
+        uint256 expectedLoops = Math.min(expectedPromoted.divUp(1e18), maxLoops);
+
+        assertEq(promoted, expectedPromoted, "promoted");
+        assertEq(totalP2PBorrow, promoted, "total borrow");
+        assertEq(loopsDone, expectedLoops, "loops");
+    }
+
+    function testDemoteSuppliers(uint256 numSuppliers, uint256 amountToMatch, uint256 maxLoops) public {
+        numSuppliers = bound(numSuppliers, 0, 10);
+        amountToMatch = bound(amountToMatch, 0, 20e18);
+        maxLoops = bound(maxLoops, 0, numSuppliers);
+
+        Types.MarketBalances storage marketBalances = _marketBalances[dai];
+
+        for (uint256 i; i < numSuppliers; i++) {
+            _updateSupplierInDS(dai, address(uint160(i + 1)), 0, 1e18);
+        }
+
+        uint256 demoted = _demoteSuppliers(dai, amountToMatch, maxLoops);
+
+        uint256 totalP2PSupply;
+        for (uint256 i; i < numSuppliers; i++) {
+            address user = address(uint160(i + 1));
+            assertEq(
+                marketBalances.scaledPoolSupplyBalance(user) + marketBalances.scaledP2PSupplyBalance(user),
+                1e18,
+                "user supply"
+            );
+            totalP2PSupply += marketBalances.scaledP2PSupplyBalance(user);
+        }
+
+        uint256 expectedDemoted = Math.min(amountToMatch, maxLoops * 1e18);
+        expectedDemoted = Math.min(expectedDemoted, numSuppliers * 1e18);
+
+        assertEq(demoted, expectedDemoted, "demoted");
+        assertEq(totalP2PSupply, 1e18 * numSuppliers - demoted, "total borrow");
+    }
+
+    function testDemoteBorrowers(uint256 numBorrowers, uint256 amountToMatch, uint256 maxLoops) public {
+        numBorrowers = bound(numBorrowers, 0, 10);
+        amountToMatch = bound(amountToMatch, 0, 20e18);
+        maxLoops = bound(maxLoops, 0, numBorrowers);
+
+        Types.MarketBalances storage marketBalances = _marketBalances[dai];
+
+        for (uint256 i; i < numBorrowers; i++) {
+            _updateBorrowerInDS(dai, address(uint160(i + 1)), 0, 1e18);
+        }
+
+        uint256 demoted = _demoteBorrowers(dai, amountToMatch, maxLoops);
+
+        uint256 totalP2PBorrow;
+        for (uint256 i; i < numBorrowers; i++) {
+            address user = address(uint160(i + 1));
+            assertEq(
+                marketBalances.scaledPoolBorrowBalance(user) + marketBalances.scaledP2PBorrowBalance(user),
+                1e18,
+                "user borrow"
+            );
+            totalP2PBorrow += marketBalances.scaledP2PBorrowBalance(user);
+        }
+
+        uint256 expectedDemoted = Math.min(amountToMatch, maxLoops * 1e18);
+        expectedDemoted = Math.min(expectedDemoted, numBorrowers * 1e18);
+
+        assertEq(demoted, expectedDemoted, "demoted");
+        assertEq(totalP2PBorrow, 1e18 * numBorrowers - demoted, "total borrow");
     }
 }
