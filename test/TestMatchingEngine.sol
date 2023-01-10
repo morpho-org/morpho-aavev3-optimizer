83b3d1fd39de2c44c3d21c6fa4ff193463b2e666
Patrick Kim <patrick@morpho.xyz>
2023-01-09 21:37:35 -0500

    chore: remove unused imports and libs from matching engine test

--- test/TestMatchingEngine.sol
+++ test/TestMatchingEngine.sol
@@ -1,44 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import {TestHelpers} from "./helpers/TestHelpers.sol";
 import {TestConfig} from "./helpers/TestConfig.sol";
-
 import {TestSetup} from "./setup/TestSetup.sol";
-import {console2} from "@forge-std/console2.sol";
 
 import {WadRayMath} from "@morpho-utils/math/WadRayMath.sol";
-import {PercentageMath} from "@morpho-utils/math/PercentageMath.sol";
-import {ThreeHeapOrdering} from "@morpho-data-structures/ThreeHeapOrdering.sol";
-
-import {SafeTransferLib, ERC20} from "@solmate/utils/SafeTransferLib.sol";
-import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-
-import {IPool, IPoolAddressesProvider} from "../src/interfaces/aave/IPool.sol";
-import {IPriceOracleGetter} from "@aave/core-v3/contracts/interfaces/IPriceOracleGetter.sol";
-import {DataTypes} from "../src/libraries/aave/DataTypes.sol";
-import {ReserveConfiguration} from "../src/libraries/aave/ReserveConfiguration.sol";
+import {Math} from "@morpho-utils/math/Math.sol";
 
 import {MatchingEngine} from "../src/MatchingEngine.sol";
 import {MorphoInternal} from "../src/MorphoInternal.sol";
 import {MorphoStorage} from "../src/MorphoStorage.sol";
+
 import {Types} from "../src/libraries/Types.sol";
 import {MarketLib} from "../src/libraries/MarketLib.sol";
 import {MarketBalanceLib} from "../src/libraries/MarketBalanceLib.sol";
-import {PoolLib} from "../src/libraries/PoolLib.sol";
-import {Math} from "@morpho-utils/math/Math.sol";
 
 contract TestMatchingEngine is TestSetup, MatchingEngine {
     using MarketLib for Types.Market;
     using MarketBalanceLib for Types.MarketBalances;
-    using PoolLib for IPool;
     using WadRayMath for uint256;
-    using PercentageMath for uint256;
-    using SafeTransferLib for ERC20;
-    using ThreeHeapOrdering for ThreeHeapOrdering.HeapArray;
     using TestConfig for TestConfig.Config;
-    using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
-    using EnumerableSet for EnumerableSet.AddressSet;
     using Math for uint256;
 
     constructor() TestSetup() MorphoStorage(config.load(vm.envString("NETWORK")).getAddress("addressesProvider")) {}
