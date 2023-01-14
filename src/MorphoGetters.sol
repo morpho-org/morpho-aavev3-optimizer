5c9cdceadb9f2834cd1e5808c3063478ab88becd
MerlinEgalite <egalite.merlin@gmail.com>
2023-01-14 15:57:54 +0100

    feat: add balance getters

--- src/MorphoGetters.sol
+++ src/MorphoGetters.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.17;
 import {IMorphoGetters} from "./interfaces/IMorpho.sol";
 
 import {Types} from "./libraries/Types.sol";
+import {MarketLib} from "./libraries/MarketLib.sol";
+import {WadRayMath} from "@morpho-utils/math/WadRayMath.sol";
 import {MarketBalanceLib} from "./libraries/MarketBalanceLib.sol";
 
 import {MorphoInternal} from "./MorphoInternal.sol";
 
 abstract contract MorphoGetters is IMorphoGetters, MorphoInternal {
+    using WadRayMath for uint256;
+    using MarketLib for Types.Market;
     using MarketBalanceLib for Types.MarketBalances;
 
     /// STORAGE ///
@@ -51,6 +55,24 @@ abstract contract MorphoGetters is IMorphoGetters, MorphoInternal {
 
     function scaledCollateralBalance(address underlying, address user) external view returns (uint256) {
         return _marketBalances[underlying].scaledCollateralBalance(user);
+    }
+
+    function supplyBalance(address underlying, address user) external view returns (uint256) {
+        Types.MarketSideIndexes256 memory indexes = _market[underlying].getSupplyIndexes();
+        return _marketBalances[underlying].scaledPoolSupplyBalance(user).rayMul(indexes.poolIndex)
+            + _marketBalances[underlying].scaledP2PSupplyBalance(user).rayMul(indexes.p2pIndex);
+    }
+
+    function borrowBalance(address underlying, address user) external view returns (uint256) {
+        Types.MarketSideIndexes256 memory indexes = _market[underlying].getBorrowIndexes();
+        return _marketBalances[underlying].scaledPoolBorrowBalance(user).rayMul(indexes.poolIndex)
+            + _marketBalances[underlying].scaledP2PBorrowBalance(user).rayMul(indexes.p2pIndex);
+    }
+
+    function collateralBalance(address underlying, address user) external view returns (uint256) {
+        return _marketBalances[underlying].scaledCollateralBalance(user).rayMul(
+            _market[underlying].indexes.supply.poolIndex
+        );
     }
 
     function isManaging(address owner, address manager) external view returns (bool) {
