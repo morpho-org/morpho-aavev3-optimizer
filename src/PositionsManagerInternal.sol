b1c0c915efef01ed7e6f55644952607195b1bc25
Rubilmax <rmilon@gmail.com>
2023-01-20 16:22:32 +0100

    refactor(imports): remove unnecessary import

--- src/PositionsManagerInternal.sol
+++ src/PositionsManagerInternal.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import {IAaveOracle} from "@aave-v3-core/interfaces/IAaveOracle.sol";
 import {IPriceOracleSentinel} from "@aave-v3-core/interfaces/IPriceOracleSentinel.sol";
 
 import {Types} from "./libraries/Types.sol";
