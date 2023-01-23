54851ac255bbc08256d90b7c32e3013eab469657
MerlinEgalite <egalite.merlin@gmail.com>
2023-01-23 23:54:11 +0100

    refactor: simplify permit2lib

--- src/libraries/Permit2Lib.sol
+++ src/libraries/Permit2Lib.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import {IPermit2} from "../interfaces/IPermit2.sol";
-import {IDAIPermit} from "../interfaces/IDAIPermit.sol";
 import {IAllowanceTransfer} from "../interfaces/IAllowanceTransfer.sol";
 
 import {SafeCast160} from "./SafeCast160.sol";
@@ -10,22 +9,16 @@ import {SafeCast160} from "./SafeCast160.sol";
 import {ERC20} from "@solmate/tokens/ERC20.sol";
 
 /// @title Permit2Lib
-/// @notice Enables efficient transfers and EIP-2612/DAI
-/// permits for any token by falling back to Permit2.
+/// @notice Enables efficient transfers permits for any token by using Permit2.
 library Permit2Lib {
     using SafeCast160 for uint256;
-    /*//////////////////////////////////////////////////////////////
-                                CONSTANTS
-    //////////////////////////////////////////////////////////////*/
 
-    /// @dev The unique EIP-712 domain domain separator for the DAI token contract.
-    bytes32 internal constant DAI_DOMAIN_SEPARATOR = 0xdbb8cf42e1ecb028be3f3dbc922e1d878b963f411dc388ced501601c60f7c6f7;
-
-    /// @dev The address for the WETH9 contract on Ethereum mainnet, encoded as a bytes32.
-    bytes32 internal constant WETH9_ADDRESS = 0x000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
+    /// CONSTANTS ///
 
     /// @dev The address of the Permit2 contract the library will use.
     IPermit2 internal constant PERMIT2 = IPermit2(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
+
+    /// INTERNAL ///
 
     /// @notice Transfer a given amount of tokens from one user to another.
     /// @param token The token to transfer.
@@ -54,10 +47,6 @@ library Permit2Lib {
         if (!success) PERMIT2.transferFrom(from, to, amount.toUint160(), address(token));
     }
 
-    /*//////////////////////////////////////////////////////////////
-                              PERMIT LOGIC
-    //////////////////////////////////////////////////////////////*/
-
     /// @notice Permit a user to spend a given amount of
     /// another user's tokens via the owner's EIP-712 signature.
     /// @param token The token to permit spending.
@@ -78,66 +67,23 @@ library Permit2Lib {
         bytes32 r,
         bytes32 s
     ) internal {
-        // Generate calldata for a call to DOMAIN_SEPARATOR on the token.
-        bytes memory inputData = abi.encodeWithSelector(ERC20.DOMAIN_SEPARATOR.selector);
+        IAllowanceTransfer.PackedAllowance memory packedAllowance = PERMIT2.allowance(owner, address(token), spender);
 
-        bool success; // Call the token contract as normal, capturing whether it succeeded.
-        bytes32 domainSeparator; // If the call succeeded, we'll capture the return value here.
-
-        assembly {
-            // If the token is WETH9, we know it doesn't have a DOMAIN_SEPARATOR, and we'll skip this step.
-            // We make sure to mask the token address as its higher order bits aren't guaranteed to be clean.
-            if iszero(eq(and(token, 0xffffffffffffffffffffffffffffffffffffffff), WETH9_ADDRESS)) {
-                success :=
-                    and(
-                        // Should resolve false if its not 32 bytes or its first word is 0.
-                        and(iszero(iszero(mload(0))), eq(returndatasize(), 32)),
-                        // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
-                        // Counterintuitively, this call must be positioned second to the and() call in the
-                        // surrounding and() call or else returndatasize() will be zero during the computation.
-                        // We send a maximum of 5000 gas to prevent tokens with fallbacks from using a ton of gas.
-                        // which should be plenty to allow tokens to fetch their DOMAIN_SEPARATOR from storage, etc.
-                        staticcall(5000, token, add(inputData, 32), mload(inputData), 0, 32)
-                    )
-
-                domainSeparator := mload(0) // Copy the return value into the domainSeparator variable.
-            }
-        }
-
-        // If the call to DOMAIN_SEPARATOR succeeded, try using permit on the token.
-        if (success) {
-            // We'll use DAI's special permit if it's DOMAIN_SEPARATOR matches,
-            // otherwise we'll just encode a call to the standard permit function.
-            inputData = domainSeparator == DAI_DOMAIN_SEPARATOR
-                ? abi.encodeCall(IDAIPermit.permit, (owner, spender, token.nonces(owner), deadline, true, v, r, s))
-                : abi.encodeCall(ERC20.permit, (owner, spender, amount, deadline, v, r, s));
-
-            assembly {
-                success := call(gas(), token, 0, add(inputData, 32), mload(inputData), 0, 0)
-            }
-        }
-
-        if (!success) {
-            // If the initial DOMAIN_SEPARATOR call on the token failed or a
-            // subsequent call to permit failed, fall back to using Permit2.
-
-            IAllowanceTransfer.PackedAllowance memory packedAllowance =
-                PERMIT2.allowance(owner, address(token), spender);
-
-            IAllowanceTransfer.PermitDetails memory details = IAllowanceTransfer.PermitDetails({
-                token: address(token),
-                amount: amount.toUint160(),
-                // Use an unlimited expiration because it most
-                // closely mimics how a standard approval works.
-                expiration: type(uint48).max,
-                nonce: packedAllowance.nonce
-            });
-
-            PERMIT2.permit(
-                owner,
-                IAllowanceTransfer.PermitSingle({details: details, spender: spender, sigDeadline: deadline}),
-                bytes.concat(r, s, bytes1(v))
-            );
-        }
+        PERMIT2.permit(
+            owner,
+            IAllowanceTransfer.PermitSingle({
+                details: IAllowanceTransfer.PermitDetails({
+                    token: address(token),
+                    amount: amount.toUint160(),
+                    // Use an unlimited expiration because it most
+                    // closely mimics how a standard approval works.
+                    expiration: type(uint48).max,
+                    nonce: packedAllowance.nonce
+                }),
+                spender: spender,
+                sigDeadline: deadline
+            }),
+            bytes.concat(r, s, bytes1(v))
+        );
     }
 }
