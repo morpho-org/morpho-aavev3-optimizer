52ff46c31d8f6386e5578e644cbe8aa18959a40c
Rubilmax <rmilon@gmail.com>
2023-01-25 10:28:57 +0100

    refactor(test-supply): adapt supply integration test

--- test/unit/TestMarketBalanceLib.sol
+++ test/unit/TestMarketBalanceLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 import {Types} from "src/libraries/Types.sol";
 import {MarketBalanceLib} from "src/libraries/MarketBalanceLib.sol";
