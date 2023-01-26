0065d7e4e77eb31debd23614b38977634626ed8a
MerlinEgalite <egalite.merlin@gmail.com>
2023-01-26 17:22:13 +0100

    refactor: shorten sums

--- src/libraries/DeltasLib.sol
+++ src/libraries/DeltasLib.sol
@@ -38,8 +38,8 @@ library DeltasLib {
         Types.MarketSideIndexes256 memory counterIndexes = borrowSide ? indexes.supply : indexes.borrow;
 
         p2pBalanceIncrease = amount.rayDiv(counterIndexes.p2pIndex);
-        promotedDelta.scaledTotalP2P = promotedDelta.scaledTotalP2P + promoted.rayDiv(promotedIndexes.p2pIndex);
-        counterDelta.scaledTotalP2P = counterDelta.scaledTotalP2P + p2pBalanceIncrease;
+        promotedDelta.scaledTotalP2P += promoted.rayDiv(promotedIndexes.p2pIndex);
+        counterDelta.scaledTotalP2P += p2pBalanceIncrease;
 
         emit Events.P2PTotalsUpdated(underlying, deltas.supply.scaledTotalP2P, deltas.borrow.scaledTotalP2P);
     }
