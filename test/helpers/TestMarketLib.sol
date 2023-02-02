367ddfc1c743e0774fb56e3578dc8f075675457e
Rubilmax <rmilon@gmail.com>
2023-02-02 13:42:44 +0100

    refactor(test-market-lib): add variable/stable borrow utils

--- test/helpers/TestMarketLib.sol
+++ test/helpers/TestMarketLib.sol
@@ -9,7 +9,8 @@ import {Vm} from "@forge-std/Vm.sol";
 
 struct TestMarket {
     address aToken;
-    address debtToken;
+    address variableDebtToken;
+    address stableDebtToken;
     address underlying;
     string symbol;
     uint256 decimals;
@@ -37,17 +38,17 @@ library TestMarketLib {
     /// @dev Disables the same block borrow/repay limitation by resetting the previous index of the given user on AaveV3.
     /// https://github.com/aave/aave-v3-core/blob/bb625723211944a7325b505caf6199edf4b8ed2a/contracts/protocol/tokenization/base/IncentivizedERC20.sol#L41-L50
     /// The check was removed in commit https://github.com/aave/aave-v3-core/commit/b1d94da8c4de795e94a7ff5c1429a98854cb2b65 but the change is not deployed at test block.
-    function _resetPreviousIndex(address debtToken, address user) private {
+    function _resetPreviousIndex(address variableDebtToken, address user) private {
         bytes32 slot = keccak256(abi.encode(user, 56));
         vm.store(
-            debtToken,
+            variableDebtToken,
             slot,
-            vm.load(debtToken, slot) & 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff
+            vm.load(variableDebtToken, slot) & 0x00000000000000000000000000000000ffffffffffffffffffffffffffffffff
         );
     }
 
     function resetPreviousIndex(TestMarket storage market, address user) internal {
-        _resetPreviousIndex(market.debtToken, user);
+        _resetPreviousIndex(market.variableDebtToken, user);
     }
 
     /// @dev Returns the quantity that can be borrowed/withdrawn from the market.
@@ -60,9 +61,19 @@ library TestMarketLib {
         return ERC20(market.aToken).totalSupply();
     }
 
-    /// @dev Returns the quantity currently borrowed from the market on AaveV3.
+    /// @dev Returns the quantity currently borrowed (with variable & stable rates) on the market on AaveV3.
     function totalBorrow(TestMarket storage market) internal view returns (uint256) {
-        return ERC20(market.debtToken).totalSupply();
+        return totalVariableBorrow(market) + totalStableBorrow(market);
+    }
+
+    /// @dev Returns the quantity currently borrowed with variable rate from the market on AaveV3.
+    function totalVariableBorrow(TestMarket storage market) internal view returns (uint256) {
+        return ERC20(market.variableDebtToken).totalSupply();
+    }
+
+    /// @dev Returns the quantity currently borrowed with stable rate from the market on AaveV3.
+    function totalStableBorrow(TestMarket storage market) internal view returns (uint256) {
+        return ERC20(market.stableDebtToken).totalSupply();
     }
 
     /// @dev Returns the quantity currently supplied on behalf of the user, on the market on AaveV3.
@@ -70,9 +81,14 @@ library TestMarketLib {
         return ERC20(market.aToken).balanceOf(user);
     }
 
-    /// @dev Returns the quantity currently borrowed on behalf of the user, on the market on AaveV3.
-    function borrowOf(TestMarket storage market, address user) internal view returns (uint256) {
-        return ERC20(market.debtToken).balanceOf(user);
+    /// @dev Returns the quantity currently borrowed on behalf of the user, with variable rate, on the market on AaveV3.
+    function variableBorrowOf(TestMarket storage market, address user) internal view returns (uint256) {
+        return ERC20(market.variableDebtToken).balanceOf(user);
+    }
+
+    /// @dev Returns the quantity currently borrowed on behalf of the user, with stable rate, on the market on AaveV3.
+    function stableBorrowOf(TestMarket storage market, address user) internal view returns (uint256) {
+        return ERC20(market.stableDebtToken).balanceOf(user);
     }
 
     /// @dev Calculates the underlying amount that can be supplied on the given market on AaveV3, reaching the supply cap.
