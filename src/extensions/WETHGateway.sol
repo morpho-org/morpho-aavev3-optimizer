8278c8433d4bda9fc77c7ab7ce5d09b4cc93f501
MerlinEgalite <egalite.merlin@gmail.com>
2023-02-10 10:42:21 +0100

    refactor: capital letters

--- src/extensions/WETHGateway.sol
+++ src/extensions/WETHGateway.sol
@@ -23,12 +23,12 @@ contract WETHGateway {
 
     /// IMMUTABLES ///
 
-    IMorpho internal immutable _morpho;
+    IMorpho internal immutable _MORPHO;
 
     /// CONSTRUCTOR ///
 
     constructor(address morpho) {
-        _morpho = IMorpho(morpho);
+        _MORPHO = IMorpho(morpho);
         ERC20(_WETH).safeApprove(morpho, type(uint256).max);
     }
 
@@ -40,46 +40,46 @@ contract WETHGateway {
     }
 
     /// @notice Returns the address of the Morpho address.
-    function morpho() external view returns (address) {
-        return address(_morpho);
+    function MORPHO() external view returns (address) {
+        return address(_MORPHO);
     }
 
     /// @notice Wraps `msg.value` of ETH to WETH and supplies it to the Morpho on behalf of `onBehalf`.
     function supplyETH(address onBehalf, uint256 maxIterations) external payable {
         _wrapETH(msg.value);
-        _morpho.supply(_WETH, msg.value, onBehalf, maxIterations);
+        _MORPHO.supply(_WETH, msg.value, onBehalf, maxIterations);
     }
 
     /// @notice Wraps `msg.value` of ETH to WETH and supplies it as collateral to the Morpho on behalf of `onBehalf`.
     function supplyCollateralETH(address onBehalf) external payable {
         _wrapETH(msg.value);
-        _morpho.supplyCollateral(_WETH, msg.value, onBehalf);
+        _MORPHO.supplyCollateral(_WETH, msg.value, onBehalf);
     }
 
     /// @notice Borrows WETH on behalf of `msg.sender`, unwraps it to WETH and sends it to `msg.sender`.
     ///         Note: `msg.sender` must have approved this contract to be its manager.
     function borrowETH(uint256 amount, uint256 maxIterations) external {
-        amount = _morpho.borrow(_WETH, amount, msg.sender, address(this), maxIterations);
+        amount = _MORPHO.borrow(_WETH, amount, msg.sender, address(this), maxIterations);
         _unwrapAndTransferETH(amount);
     }
 
     /// @notice Wraps `msg.value` of ETH to WETH and repays `onBehalf`'s debt on Morpho.
     function repayETH(address onBehalf) external payable {
         _wrapETH(msg.value);
-        _morpho.repay(_WETH, msg.value, onBehalf);
+        _MORPHO.repay(_WETH, msg.value, onBehalf);
     }
 
     /// @notice Withdraws WETH up to `amount` on behalf of `msg.sender`, unwraps it to WETH and sends it to `msg.sender`.
     ///         Note: `msg.sender` must have approved this contract to be its manager.
     function withdrawETH(uint256 amount, uint256 maxIterations) external {
-        amount = _morpho.withdraw(_WETH, amount, msg.sender, address(this), maxIterations);
+        amount = _MORPHO.withdraw(_WETH, amount, msg.sender, address(this), maxIterations);
         _unwrapAndTransferETH(amount);
     }
 
     /// @notice Withdraws WETH as collateral up to `amount` on behalf of `msg.sender`, unwraps it to WETH and sends it to `msg.sender`.
     ///         Note: `msg.sender` must have approved this contract to be its manager.
     function withdrawCollateralETH(uint256 amount) external {
-        amount = _morpho.withdrawCollateral(_WETH, amount, msg.sender, address(this));
+        amount = _MORPHO.withdrawCollateral(_WETH, amount, msg.sender, address(this));
         _unwrapAndTransferETH(amount);
     }
 
