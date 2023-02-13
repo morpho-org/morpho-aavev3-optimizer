a9e7d30d6b6e4a26bc320b95cc02a1dfd7626663
MathisGD <mathis.gontier_delaunay@telecom-sudparis.eu>
2023-02-13 12:43:17 +0100

    fix: test delta naming

--- test/internal/TestPositionsManagerInternalCaps.sol
+++ test/internal/TestPositionsManagerInternalCaps.sol
@@ -51,7 +51,7 @@ contract TestInternalPositionsManagerInternalCaps is InternalTest, PositionsMana
 
         poolAdmin.setBorrowCap(dai, 0);
 
-        market.deltas.borrow.scaledDeltaPool = delta.rayDiv(indexes.borrow.poolIndex);
+        market.deltas.borrow.scaledP2PDelta = delta.rayDiv(indexes.borrow.poolIndex);
         market.deltas.borrow.scaledTotalP2P = totalP2P.rayDiv(indexes.borrow.p2pIndex);
 
         this.authorizeBorrow(dai, amount);
@@ -85,7 +85,7 @@ contract TestInternalPositionsManagerInternalCaps is InternalTest, PositionsMana
 
         poolAdmin.setBorrowCap(dai, borrowCap);
 
-        market.deltas.borrow.scaledDeltaPool = delta.rayDiv(indexes.borrow.poolIndex);
+        market.deltas.borrow.scaledP2PDelta = delta.rayDiv(indexes.borrow.poolIndex);
         market.deltas.borrow.scaledTotalP2P = totalP2P.rayDiv(indexes.borrow.p2pIndex);
 
         vm.expectRevert(abi.encodeWithSelector(Errors.ExceedsBorrowCap.selector));
