b8b257ebd4dc982d15be6020a7c9eaaf269386ed
Patrick Kim <patrick@morpho.xyz>
2023-02-17 17:07:12 -0500

    test: address some comments and split increase idle supply test

--- test/integration/TestIntegrationMarketIdle.sol
+++ test/integration/TestIntegrationMarketIdle.sol
@@ -19,13 +19,16 @@ contract TestIntegrationMarketIdle is IntegrationTest {
     using ReserveConfiguration for DataTypes.ReserveConfigurationMap;
     using MarketLib for Types.Market;
     using Math for uint256;
+    using TestMarketLib for TestMarket;
 
     Types.Market internal market;
 
-    function testIncreaseIdleWhenSupplyCapIsMax(Types.Market memory _market, uint256 amount) public {
+    function testIncreaseIdleWhenNoSupplyCap(Types.Market memory _market, uint256 amount) public {
+        TestMarket storage testMarket = testMarkets[dai];
+
         market = _market;
 
-        poolAdmin.setSupplyCap(dai, 0);
+        _setSupplyCap(testMarket, 0);
 
         DataTypes.ReserveData memory reserve = pool.getReserveData(dai);
         Types.Indexes256 memory indexes = morpho.updatedIndexes(dai);
@@ -35,10 +38,15 @@ contract TestIntegrationMarketIdle is IntegrationTest {
         assertEq(idleSupplyIncrease, 0, "idleSupplyIncrease");
     }
 
-    function testIncreaseIdle(Types.Market memory _market, uint256 amount, uint256 supplyCap) public {
+    function testIncreaseIdleWhenSupplyGapIsLarger(Types.Market memory _market, uint256 amount, uint256 supplyCap)
+        public
+    {
         TestMarket storage testMarket = testMarkets[dai];
-        supplyCap = _boundSupplyCapExceeded(testMarket, testMarket.minAmount * 10, supplyCap);
-        amount = bound(amount, testMarket.minAmount, testMarket.maxAmount);
+        supplyCap = _boundSupplyCapExceeded(
+            testMarket,
+            (testMarket.totalSupply() + _accruedToTreasury(testMarket.underlying) / (10 ** testMarket.decimals)) + 1,
+            ReserveConfiguration.MAX_VALID_SUPPLY_CAP
+        );
 
         _market.aToken = testMarket.aToken;
         _market.idleSupply = bound(_market.idleSupply, 0, testMarket.maxAmount);
@@ -48,15 +56,42 @@ contract TestIntegrationMarketIdle is IntegrationTest {
         _setSupplyCap(testMarket, supplyCap);
 
         uint256 supplyGap = _supplyGap(testMarket);
-        uint256 expectedIdleIncrease = amount.zeroFloorSub(supplyGap);
+        amount = bound(amount, testMarket.minAmount, supplyGap);
 
         DataTypes.ReserveData memory reserve = pool.getReserveData(dai);
         Types.Indexes256 memory indexes = morpho.updatedIndexes(dai);
 
-        if (amount > supplyGap) {
-            vm.expectEmit(true, true, true, true);
-            emit Events.IdleSupplyUpdated(testMarket.underlying, _market.idleSupply + expectedIdleIncrease);
-        }
+        (uint256 suppliable, uint256 idleSupplyIncrease) = market.increaseIdle(dai, amount, reserve, indexes);
+
+        assertEq(suppliable, amount, "suppliable");
+        assertEq(idleSupplyIncrease, 0, "idleSupplyIncrease");
+        assertEq(market.idleSupply, _market.idleSupply, "market.idleSupply");
+    }
+
+    function testIncreaseIdleWhenSupplyGapIsSmaller(Types.Market memory _market, uint256 amount, uint256 supplyCap)
+        public
+    {
+        TestMarket storage testMarket = testMarkets[dai];
+        supplyCap = _boundSupplyCapExceeded(testMarket, testMarket.minAmount * 10, supplyCap);
+
+        _market.aToken = testMarket.aToken;
+        _market.idleSupply = bound(_market.idleSupply, 0, testMarket.maxAmount);
+
+        market = _market;
+
+        _setSupplyCap(testMarket, supplyCap);
+
+        uint256 supplyGap = _supplyGap(testMarket);
+
+        amount = bound(amount, supplyGap, testMarket.maxAmount);
+        uint256 expectedIdleIncrease = amount - supplyGap;
+
+        DataTypes.ReserveData memory reserve = pool.getReserveData(dai);
+        Types.Indexes256 memory indexes = morpho.updatedIndexes(dai);
+
+        vm.expectEmit(true, true, true, true);
+        emit Events.IdleSupplyUpdated(testMarket.underlying, _market.idleSupply + expectedIdleIncrease);
+
         (uint256 suppliable, uint256 idleSupplyIncrease) = market.increaseIdle(dai, amount, reserve, indexes);
 
         assertEq(suppliable, supplyGap, "suppliable");
