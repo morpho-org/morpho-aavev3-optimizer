0f65efdcbbf44efc683a5bb30e8a2f1847e120d8
Tristan22400 <tristanmartin22400@gmail.com>
2023-02-21 11:43:44 +0100

    fix: remove useless set up

--- test/integration/TestIntegrationMorphoUserEmode.sol
+++ test/integration/TestIntegrationMorphoUserEmode.sol
@@ -3,10 +3,6 @@ pragma solidity ^0.8.0;
 import "test/helpers/IntegrationTest.sol";
 
 contract TestIntegrationMorphoEMode is IntegrationTest {
-    function setUp() public virtual override {
-        super.setUp();
-    }
-
     function testMorphoEModeOnAave() public {
         assertEq(E_MODE_CATEGORY_ID, pool.getUserEMode(address(morphoProxy)));
     }
