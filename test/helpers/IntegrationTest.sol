3f5101d683de4d3fabf7bbd76f660b082bfe5f41
Patrick Kim <patrick@morpho.xyz>
2023-02-21 14:27:06 -0500

    test: componentize total supply to cap

--- test/helpers/IntegrationTest.sol
+++ test/helpers/IntegrationTest.sol
@@ -152,15 +152,16 @@ contract IntegrationTest is ForkTest {
         morpho.createMarket(market.underlying, market.reserveFactor, market.p2pIndexCursor);
     }
 
+    /// @dev Returns the total supply used towards the supply cap.
+    function _totalSupplyToCap(TestMarket storage market) internal view returns (uint256) {
+        return (IAToken(market.aToken).scaledTotalSupply() + _accruedToTreasury(market.underlying)).rayMul(
+            pool.getReserveNormalizedIncome(market.underlying)
+        );
+    }
+
     /// @dev Calculates the underlying amount that can be supplied on the given market on AaveV3, reaching the supply cap.
     function _supplyGap(TestMarket storage market) internal view returns (uint256) {
-        return market.supplyCap.zeroFloorSub(
-            (
-                (IAToken(market.aToken).scaledTotalSupply() + _accruedToTreasury(market.underlying)).rayMul(
-                    pool.getReserveNormalizedIncome(market.underlying)
-                )
-            )
-        );
+        return market.supplyCap.zeroFloorSub(_totalSupplyToCap(market));
     }
 
     /// @dev Sets the supply cap of AaveV3 to the given input.
@@ -173,9 +174,7 @@ contract IntegrationTest is ForkTest {
     /// @dev Sets the supply gap of AaveV3 to the given input.
     /// @return The new supply gap after rounding since supply caps on AAVE are only granular up to the token's decimals.
     function _setSupplyGap(TestMarket storage market, uint256 supplyGap) internal returns (uint256) {
-        _setSupplyCap(
-            market, (market.totalSupply() + _accruedToTreasury(market.underlying) + supplyGap) / (10 ** market.decimals)
-        );
+        _setSupplyCap(market, (_totalSupplyToCap(market) + supplyGap) / (10 ** market.decimals));
         return _supplyGap(market);
     }
 
@@ -224,11 +223,7 @@ contract IntegrationTest is ForkTest {
         view
         returns (uint256)
     {
-        return bound(
-            supplyCap,
-            1,
-            (market.totalSupply() + _accruedToTreasury(market.underlying) + amount) / (10 ** market.decimals)
-        );
+        return bound(supplyCap, 1, (_totalSupplyToCap(market) + amount) / (10 ** market.decimals));
     }
 
     /// @dev Bounds the input borrow cap of AaveV3 so that it is exceeded after having deposited a given amount
