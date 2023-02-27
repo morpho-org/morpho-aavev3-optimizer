b4c3ccee45c84eb2b14530d7cb032fac5393b084
Tristan22400 <tristanmartin22400@gmail.com>
2023-02-27 12:08:31 +0100

    fix: assumption

--- test/integration/TestIntegrationFee.sol
+++ test/integration/TestIntegrationFee.sol
@@ -80,11 +80,12 @@ contract TestIntegrationFee is IntegrationTest {
             );
 
             vm.warp(block.timestamp + (365 days));
-            uint256 beforeBalance = ERC20(testMarket.underlying).balanceOf(address(morpho));
-
             Types.Market memory market = morpho.market(testMarket.underlying);
             Types.Deltas memory deltas = market.deltas;
             Types.Indexes256 memory indexes = morpho.updatedIndexes(testMarket.underlying);
+
+            uint256 beforeBalance = ERC20(testMarket.underlying).balanceOf(address(morpho));
+
             uint256 expectedFeeCollected = deltas.borrow.scaledP2PTotal.rayMul(indexes.borrow.p2pIndex).zeroFloorSub(
                 deltas.supply.scaledP2PTotal.rayMul(indexes.supply.p2pIndex).zeroFloorSub(
                     deltas.supply.scaledDelta.rayMul(indexes.supply.poolIndex)
@@ -108,59 +109,51 @@ contract TestIntegrationFee is IntegrationTest {
     function testRepayFeeWithBorrowDeltaAndP2P(uint16 reserveFactor, uint256 borrowDeltaAmount, uint256 borrowAmount)
         public
     {
-        TestMarket storage testMarket = testMarkets[borrowableUnderlyings[0]];
-        reserveFactor = uint16(bound(reserveFactor, 0, PercentageMath.PERCENTAGE_FACTOR));
-        morpho.setReserveFactor(testMarket.underlying, reserveFactor);
-        borrowDeltaAmount = bound(borrowDeltaAmount, 0, type(uint96).max - 1);
-        console.log("delta");
-        borrowAmount = bound(borrowAmount, borrowDeltaAmount, type(uint96).max);
-        borrowAmount = _boundBorrow(testMarket, borrowAmount);
-        borrowAmount = _promoteBorrow(promoter1, testMarket, borrowAmount);
+        for (uint256 marketIndex; marketIndex < 1; ++marketIndex) {
+            _revert();
+            TestMarket storage testMarket = testMarkets[borrowableUnderlyings[marketIndex]];
+            reserveFactor = uint16(bound(reserveFactor, 0, PercentageMath.PERCENTAGE_FACTOR));
+            morpho.setReserveFactor(testMarket.underlying, reserveFactor);
+            borrowDeltaAmount = bound(borrowDeltaAmount, 0, type(uint128).max - 1);
 
-        vm.assume(borrowDeltaAmount < borrowAmount);
-        _borrowWithoutCollateral(
-            address(user), testMarket, borrowAmount, address(user), address(user), DEFAULT_MAX_ITERATIONS
-        );
-        Types.Indexes256 memory indexes = morpho.updatedIndexes(testMarket.underlying);
-        Types.Market memory market = morpho.market(testMarket.underlying);
-        Types.Deltas memory deltas = market.deltas;
-        assertEq(deltas.borrow.scaledDelta, 0);
+            borrowAmount = bound(borrowAmount, borrowDeltaAmount, type(uint128).max);
+            borrowAmount = _boundBorrow(testMarket, borrowAmount);
+            borrowAmount = _promoteBorrow(promoter1, testMarket, borrowAmount);
 
-        borrowDeltaAmount = _increaseBorrowDelta(promoter1, testMarket, borrowDeltaAmount);
-        market = morpho.market(testMarket.underlying);
-        deltas = market.deltas;
-        indexes = morpho.updatedIndexes(testMarket.underlying);
-        assertApproxEqAbs(deltas.borrow.scaledDelta.rayMul(indexes.borrow.poolIndex), borrowDeltaAmount, 1);
+            vm.assume(borrowDeltaAmount < borrowAmount);
+            _borrowWithoutCollateral(
+                address(user), testMarket, borrowAmount, address(user), address(user), DEFAULT_MAX_ITERATIONS
+            );
 
-        vm.warp(block.timestamp + (365 days));
-        market = morpho.market(testMarket.underlying);
-        deltas = market.deltas;
-        assertApproxEqAbs(deltas.borrow.scaledDelta, borrowDeltaAmount.rayDiv(indexes.borrow.poolIndex), 1);
-        uint256 borrowBalance = morpho.borrowBalance(market.underlying, address(user));
-        uint256 beforeBalance = ERC20(testMarket.underlying).balanceOf(address(morpho));
+            borrowDeltaAmount = _increaseBorrowDelta(promoter1, testMarket, borrowDeltaAmount);
 
-        indexes = morpho.updatedIndexes(testMarket.underlying);
+            vm.warp(block.timestamp + (365 days));
 
-        vm.assume(borrowBalance > deltas.borrow.scaledDelta);
+            Types.Indexes256 memory indexes = morpho.updatedIndexes(testMarket.underlying);
+            Types.Market memory market = morpho.market(testMarket.underlying);
+            Types.Deltas memory deltas = market.deltas;
 
-        uint256 expectedFeeCollected = deltas.borrow.scaledP2PTotal.rayMul(indexes.borrow.p2pIndex).zeroFloorSub(
-            deltas.borrow.scaledDelta.rayMul(indexes.borrow.poolIndex)
-                + deltas.supply.scaledP2PTotal.rayMul(indexes.supply.p2pIndex).zeroFloorSub(
-                    deltas.supply.scaledDelta.rayMul(indexes.supply.poolIndex)
-                )
-        );
+            uint256 borrowBalance = morpho.borrowBalance(market.underlying, address(user));
+            uint256 beforeBalance = ERC20(testMarket.underlying).balanceOf(address(morpho));
 
-        console.log(deltas.borrow.scaledP2PTotal, deltas.borrow.scaledDelta, borrowDeltaAmount);
+            vm.assume(borrowBalance > deltas.borrow.scaledDelta.rayMul(indexes.borrow.poolIndex));
 
-        user.approve(market.underlying, borrowBalance);
-        user.repay(market.underlying, borrowBalance);
-        console.log(borrowBalance, deltas.supply.scaledDelta.rayMul(indexes.supply.poolIndex));
-        console.log(expectedFeeCollected, beforeBalance, ERC20(testMarket.underlying).balanceOf(address(morpho)));
-        assertApproxEqAbs(
-            ERC20(testMarket.underlying).balanceOf(address(morpho)),
-            beforeBalance + expectedFeeCollected,
-            1,
-            "Wrong amount of fees"
-        );
+            uint256 expectedFeeCollected = deltas.borrow.scaledP2PTotal.rayMul(indexes.borrow.p2pIndex).zeroFloorSub(
+                deltas.borrow.scaledDelta.rayMul(indexes.borrow.poolIndex)
+                    + deltas.supply.scaledP2PTotal.rayMul(indexes.supply.p2pIndex).zeroFloorSub(
+                        deltas.supply.scaledDelta.rayMul(indexes.supply.poolIndex)
+                    )
+            );
+
+            user.approve(market.underlying, borrowBalance);
+            user.repay(market.underlying, borrowBalance);
+
+            assertApproxEqAbs(
+                ERC20(testMarket.underlying).balanceOf(address(morpho)),
+                beforeBalance + expectedFeeCollected,
+                2,
+                "Wrong amount of fees"
+            );
+        }
     }
 }
