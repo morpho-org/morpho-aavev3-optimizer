30b2ac1768c5e164f26610220f774e434660b607
Rubilmax <rmilon@gmail.com>
2023-02-28 17:06:23 +0100

    fix(positions-manager): add aave oracle interface import

--- src/PositionsManagerInternal.sol
+++ src/PositionsManagerInternal.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.17;
 
+import {IAaveOracle} from "@aave-v3-core/interfaces/IAaveOracle.sol";
 import {IPriceOracleSentinel} from "@aave-v3-core/interfaces/IPriceOracleSentinel.sol";
 
 import {Types} from "./libraries/Types.sol";
