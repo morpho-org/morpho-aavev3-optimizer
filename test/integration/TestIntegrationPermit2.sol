bd45d49be128bccef62dbdddacb96ed5d1ed8724
Tristan22400 <tristanmartin22400@gmail.com>
2023-03-08 19:59:21 +0100

    test: permit2

--- test/integration/TestIntegrationPermit2.sol
+++ test/integration/TestIntegrationPermit2.sol
@@ -1,89 +1,152 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-import {IAllowanceTransfer} from "@permit2/AllowanceTransfer.sol";
+import {AllowanceTransfer} from "@permit2/AllowanceTransfer.sol";
+import "@permit2/libraries/PermitHash.sol";
 import {SafeCast160} from "@permit2/libraries/SafeCast160.sol";
-import {console2} from "@forge-std/console2.sol";
-import {console} from "@forge-std/console.sol";
-import {SigUtils} from "test/helpers/SigUtils.sol";
+
 import "test/helpers/IntegrationTest.sol";
 
 contract TestIntegrationPermit2 is IntegrationTest {
-    SigUtils internal sigUtils;
+    using SafeTransferLib for ERC20;
 
-    bytes32 public constant _PERMIT_DETAILS_TYPEHASH =
-        keccak256("PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)");
-
-    bytes32 public constant _PERMIT_SINGLE_TYPEHASH = keccak256(
-        "PermitSingle(PermitDetails details,address spender,uint256 sigDeadline)PermitDetails(address token,uint160 amount,uint48 expiration,uint48 nonce)"
-    );
+    AllowanceTransfer internal constant PERMIT2 = AllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
 
     function setUp() public override {
         super.setUp();
-
-        sigUtils = new SigUtils(morpho.DOMAIN_SEPARATOR());
-    }
-
-    function defaultERC20PermitAllowance(
-        address token0,
-        uint160 amount,
-        uint48 expiration,
-        uint256 nonce,
-        uint256 deadline
-    ) internal view returns (IAllowanceTransfer.PermitSingle memory) {
-        IAllowanceTransfer.PermitDetails memory details = IAllowanceTransfer.PermitDetails({
-            token: token0,
-            amount: amount,
-            expiration: expiration,
-            nonce: uint48(nonce)
-        });
-        return IAllowanceTransfer.PermitSingle({details: details, spender: address(morpho), sigDeadline: deadline});
-    }
-
-    function getPermitSignatureRaw(
-        IAllowanceTransfer.PermitSingle memory permit,
-        uint256 privateKey,
-        bytes32 domainSeparator
-    ) internal pure returns (uint8 v, bytes32 r, bytes32 s, bytes32 msgHash) {
-        bytes32 permitHash = keccak256(abi.encode(_PERMIT_DETAILS_TYPEHASH, permit.details));
-
-        bytes32 msgHash = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                domainSeparator,
-                keccak256(abi.encode(_PERMIT_SINGLE_TYPEHASH, permitHash, permit.spender, permit.sigDeadline))
-            )
-        );
-
-        (v, r, s) = vm.sign(privateKey, msgHash);
     }
 
     function testSupplyWithPermit2(uint256 privateKey, uint256 deadline, uint256 amount) public {
         vm.assume(block.timestamp < deadline);
-        privateKey = _boundAmountNotZero(privateKey);
+        privateKey = bound(privateKey, 1000, type(uint160).max);
         address delegator = vm.addr(privateKey);
         vm.assume(delegator != address(0));
 
-        console.log(delegator, privateKey);
         TestMarket storage market = testMarkets[underlyings[0]];
 
         amount = _boundSupply(market, amount);
-        uint160 supplied = uint160(Math.min(type(uint160).max, amount));
+        amount = Math.min(type(uint160).max, amount);
 
-        uint256 nounce = morpho.userNonce(delegator);
+        address spender = address(morpho);
 
-        IAllowanceTransfer.PermitSingle memory permit =
-            defaultERC20PermitAllowance(market.underlying, supplied, type(uint48).max, nounce, deadline);
-        bytes32 msgHash;
+        (,, uint48 nonce) = PERMIT2.allowance(delegator, address(market.underlying), spender);
+        IAllowanceTransfer.PermitDetails memory details = IAllowanceTransfer.PermitDetails({
+            token: address(market.underlying),
+            amount: SafeCast160.toUint160(amount),
+            // Use an unlimited expiration because it most
+            // closely mimics how a standard approval works.
+            expiration: type(uint48).max,
+            nonce: nonce
+        });
+        IAllowanceTransfer.PermitSingle memory permitSingle =
+            IAllowanceTransfer.PermitSingle({details: details, spender: spender, sigDeadline: deadline});
+
+        bytes32 hashed = PermitHash.hash(permitSingle);
+        hashed = keccak256(abi.encodePacked("\x19\x01", PERMIT2.DOMAIN_SEPARATOR(), hashed));
+
         Types.Signature memory sig;
-        (sig.v, sig.r, sig.s, msgHash) = getPermitSignatureRaw(permit, privateKey, morpho.DOMAIN_SEPARATOR());
-        address signer = ecrecover(msgHash, sig.v, sig.r, sig.s);
-        console.log(delegator, signer);
-        assertEq(signer, delegator);
-        console.log(permit.sigDeadline, deadline);
-        console.log(permit.spender);
-        deal(market.underlying, delegator, supplied);
+        (sig.v, sig.r, sig.s) = vm.sign(privateKey, hashed);
+
         vm.prank(delegator);
-        morpho.supplyWithPermit(market.underlying, supplied, delegator, 10, deadline, sig);
+        ERC20(market.underlying).safeApprove(address(PERMIT2), amount);
+
+        deal(market.underlying, delegator, amount);
+        assertEq(ERC20(market.underlying).balanceOf(delegator), amount);
+
+        vm.prank(delegator);
+        morpho.supplyWithPermit(market.underlying, amount, delegator, 10, deadline, sig);
+        assertApproxEqAbs(morpho.supplyBalance(market.underlying, delegator), amount, 1, "Incorrect Supply");
+    }
+
+    function testSupplyCollateralWithPermit2(uint256 privateKey, uint256 deadline, uint256 amount) public {
+        vm.assume(block.timestamp < deadline);
+        privateKey = bound(privateKey, 1000, type(uint160).max);
+        address delegator = vm.addr(privateKey);
+        vm.assume(delegator != address(0));
+
+        TestMarket storage market = testMarkets[underlyings[0]];
+
+        amount = _boundSupply(market, amount);
+        amount = Math.min(type(uint160).max, amount);
+
+        address spender = address(morpho);
+
+        (,, uint48 nonce) = PERMIT2.allowance(delegator, address(market.underlying), spender);
+        IAllowanceTransfer.PermitDetails memory details = IAllowanceTransfer.PermitDetails({
+            token: address(market.underlying),
+            amount: SafeCast160.toUint160(amount),
+            // Use an unlimited expiration because it most
+            // closely mimics how a standard approval works.
+            expiration: type(uint48).max,
+            nonce: nonce
+        });
+        IAllowanceTransfer.PermitSingle memory permitSingle =
+            IAllowanceTransfer.PermitSingle({details: details, spender: spender, sigDeadline: deadline});
+
+        bytes32 hashed = PermitHash.hash(permitSingle);
+        hashed = keccak256(abi.encodePacked("\x19\x01", PERMIT2.DOMAIN_SEPARATOR(), hashed));
+
+        Types.Signature memory sig;
+        (sig.v, sig.r, sig.s) = vm.sign(privateKey, hashed);
+
+        vm.prank(delegator);
+        ERC20(market.underlying).safeApprove(address(PERMIT2), amount);
+
+        deal(market.underlying, delegator, amount);
+        assertEq(ERC20(market.underlying).balanceOf(delegator), amount);
+
+        vm.prank(delegator);
+        morpho.supplyCollateralWithPermit(market.underlying, amount, delegator, deadline, sig);
+        assertApproxEqAbs(
+            morpho.collateralBalance(market.underlying, delegator), amount, 1, "Incorrect Supply Collateral"
+        );
+    }
+
+    function testRepayWithPermit(uint256 privateKey, uint256 deadline, uint256 amount) public {
+        vm.assume(block.timestamp < deadline);
+        privateKey = bound(privateKey, 1, type(uint160).max);
+        address delegator = vm.addr(privateKey);
+        vm.assume(delegator != address(0));
+
+        TestMarket storage market = testMarkets[underlyings[0]];
+
+        amount = _boundBorrow(market, amount);
+        amount = Math.min(type(uint160).max, amount);
+
+        address spender = address(morpho);
+
+        (,, uint48 nonce) = PERMIT2.allowance(delegator, address(market.underlying), spender);
+        IAllowanceTransfer.PermitDetails memory details = IAllowanceTransfer.PermitDetails({
+            token: address(market.underlying),
+            amount: SafeCast160.toUint160(amount),
+            // Use an unlimited expiration because it most
+            // closely mimics how a standard approval works.
+            expiration: type(uint48).max,
+            nonce: nonce
+        });
+        IAllowanceTransfer.PermitSingle memory permitSingle =
+            IAllowanceTransfer.PermitSingle({details: details, spender: spender, sigDeadline: deadline});
+
+        bytes32 hashed =
+            keccak256(abi.encodePacked("\x19\x01", PERMIT2.DOMAIN_SEPARATOR(), PermitHash.hash(permitSingle)));
+        Types.Signature memory sig;
+        (sig.v, sig.r, sig.s) = vm.sign(privateKey, hashed);
+
+        _borrowWithoutCollateral(address(delegator), market, amount, delegator, delegator, DEFAULT_MAX_ITERATIONS);
+
+        uint256 balanceBefore = morpho.borrowBalance(market.underlying, delegator);
+
+        vm.prank(delegator);
+        ERC20(market.underlying).safeApprove(address(PERMIT2), amount);
+
+        deal(market.underlying, delegator, amount);
+        assertEq(ERC20(market.underlying).balanceOf(delegator), amount);
+
+        vm.prank(delegator);
+        morpho.repayWithPermit(market.underlying, amount, delegator, deadline, sig);
+
+        assertApproxEqAbs(
+            balanceBefore, morpho.borrowBalance(market.underlying, delegator) + amount, 1, "Incorrect Borrow Balance"
+        );
     }
 }
