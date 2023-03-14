d94b2ee12d1622b5d31063c027f25d80dad86c6e
Rubilmax <rmilon@gmail.com>
2023-03-14 17:43:49 +0100

    test(invariant): wip add invariant tests

--- test/invariant/TestInvariantMorpho.sol
+++ test/invariant/TestInvariantMorpho.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import "test/helpers/InvariantTest.sol";
 
 contract TestInvariantMorpho is InvariantTest {
+    using PercentageMath for uint256;
     using SafeTransferLib for ERC20;
+    using TestMarketLib for TestMarket;
 
     bytes4[] internal selectors;
 
@@ -70,7 +72,7 @@ contract TestInvariantMorpho is InvariantTest {
     function borrow(uint256 underlyingSeed, uint256 amount, address onBehalf, address receiver, uint256 maxIterations)
         external
     {
-        TestMarket storage market = testMarkets[_randomUnderlying(underlyingSeed)];
+        TestMarket storage market = testMarkets[_randomBorrowable(underlyingSeed)];
         amount = _boundBorrow(market, amount);
         onBehalf = _randomSender(onBehalf);
         receiver = _boundReceiver(receiver);
@@ -81,7 +83,7 @@ contract TestInvariantMorpho is InvariantTest {
     }
 
     function repay(uint256 underlyingSeed, uint256 amount, address onBehalf) external {
-        TestMarket storage market = testMarkets[_randomUnderlying(underlyingSeed)];
+        TestMarket storage market = testMarkets[_randomBorrowable(underlyingSeed)];
         amount = _boundNotZero(amount);
         onBehalf = _randomSender(onBehalf);
 
@@ -148,5 +150,56 @@ contract TestInvariantMorpho is InvariantTest {
         (,,,,, uint256 healthFactor) = pool.getUserAccountData(address(morpho));
 
         assertGt(healthFactor, Constants.DEFAULT_LIQUIDATION_MAX_HF, "healthFactor");
+    }
+
+    function invariantCannotBorrowOverLtv() public {
+        address[] memory senders = targetSenders();
+
+        for (uint256 i; i < senders.length; ++i) {
+            address sender = senders[i];
+            Types.LiquidityData memory liquidityData = morpho.liquidityData(sender);
+
+            if (liquidityData.borrowable == 0) continue;
+
+            for (uint256 j; j < borrowableUnderlyings.length; ++j) {
+                TestMarket storage market = testMarkets[borrowableUnderlyings[j]];
+
+                uint256 borrowable = (liquidityData.borrowable * 1 ether * 10 ** market.decimals).percentAdd(5) // Inflate borrowable because of WBTC decimals precision.
+                    / (market.price * 1 ether);
+                if (borrowable == 0 || borrowable > market.liquidity()) continue;
+
+                vm.prank(sender);
+                vm.expectRevert(Errors.UnauthorizedBorrow.selector);
+                morpho.borrow(market.underlying, borrowable, sender, sender, 0);
+            }
+        }
+    }
+
+    function invariantCannotWithdrawOverLt() public {
+        address[] memory senders = targetSenders();
+
+        for (uint256 i; i < senders.length; ++i) {
+            address sender = senders[i];
+            address[] memory collaterals = morpho.userCollaterals(sender);
+            Types.LiquidityData memory liquidityData = morpho.liquidityData(sender);
+
+            if (liquidityData.debt == 0) continue;
+
+            for (uint256 j; j < collaterals.length; ++j) {
+                TestMarket storage market = testMarkets[collaterals[j]];
+
+                // TODO: replace with .getLt() & use rawCollateralValue()
+                uint256 withdrawable = (
+                    ((liquidityData.maxDebt - liquidityData.debt) * 1 ether * 10 ** market.decimals).percentAdd(5)
+                        / (market.price * 1 ether)
+                ) // Inflate withdrawable because of WBTC decimals precision.
+                    .percentDiv(market.lt) * Constants.LT_LOWER_BOUND / (Constants.LT_LOWER_BOUND - 1);
+                if (withdrawable == 0) continue;
+
+                vm.prank(sender);
+                vm.expectRevert(Errors.UnauthorizedWithdraw.selector);
+                morpho.withdrawCollateral(market.underlying, withdrawable, sender, sender);
+            }
+        }
     }
 }
