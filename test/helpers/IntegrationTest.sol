56bd64ca8c23987718797f4aa2e5de46e140ebd0
Patrick Kim <patrick@morpho.xyz>
2023-03-15 11:49:18 -0400

    test: hash seed for random underlyings so that we can use seeds sequentially

--- test/helpers/IntegrationTest.sol
+++ test/helpers/IntegrationTest.sol
@@ -170,11 +170,11 @@ contract IntegrationTest is ForkTest {
     }
 
     function _randomCollateral(uint256 seed) internal view returns (address) {
-        return collateralUnderlyings[seed % collateralUnderlyings.length];
+        return collateralUnderlyings[uint256(keccak256(abi.encode(seed))) % collateralUnderlyings.length];
     }
 
     function _randomBorrowable(uint256 seed) internal view returns (address) {
-        return borrowableUnderlyings[seed % borrowableUnderlyings.length];
+        return borrowableUnderlyings[uint256(keccak256(abi.encode(seed))) % borrowableUnderlyings.length];
     }
 
     /// @dev Calculates the underlying amount that can be supplied on the given market on AaveV3, reaching the supply cap.
