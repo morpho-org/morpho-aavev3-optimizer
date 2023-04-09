c70f6d0b2134cd6d26d6ed138647d8639fb93a49
Patrick Kim <patrick@morpho.xyz>
2023-04-09 13:25:42 -0400

    chore: add back removed comment in morpho

--- src/Morpho.sol
+++ src/Morpho.sol
@@ -141,6 +141,7 @@ contract Morpho is IMorpho, MorphoGetters, MorphoSetters {
     }
 
     /// @notice Repays `amount` of `underlying` on behalf of `onBehalf`.
+    ///         If sender is not `onBehalf`, sender must have previously been approved by `onBehalf` using `approveManager`.
     /// @param underlying The address of the underlying asset to borrow.
     /// @param amount The amount of `underlying` to repay.
     /// @param onBehalf The address whose position will be repaid.
